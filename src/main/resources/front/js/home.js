5e92a5663f482bb61d65cb9f84ea9d84aafdd3fe
Florian VACHER <vacherflorian43@gmail.com>
2022-11-29 10:08:51 +0100

    Add 'rayons'

--- src/main/resources/front/js/home.js
+++ src/main/resources/front/js/home.js
@@ -1,9 +1,23 @@
 function searchProduct() {
 	let input = document.getElementById("searchBar").value.toLowerCase();
-	console.log(input);
 	let products = document.getElementsByClassName('product');
 	for (let i = 0; i < products.length; i++) {
 		if (!products[i].getElementsByClassName("card-title")[0].innerHTML.toLowerCase().includes(input)) {
+			products[i].classList.add("noSearch");
+			products[i].style.visibility = "hidden";
+		}
+		else {
+			products[i].classList.remove("noSearch");
+			products[i].style.visibility = "visible";
+		}
+	}
+}
+
+function searchRayon() {
+	let input = document.getElementById("searchBar").value.toLowerCase();
+	let products = document.getElementsByClassName('rayon');
+	for (let i = 0; i < products.length; i++) {
+		if (!products[i].querySelector("h6").innerHTML.toLowerCase().includes(input)) {
 			products[i].classList.add("noSearch");
 			products[i].style.visibility = "hidden";
 		}
